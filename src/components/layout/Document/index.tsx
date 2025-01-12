8ad9d47a0bd037ba06c5b258dd0ed05b46ba1248
Behnam Azimi <bhnmzm@gmail.com>
2025-01-12 18:07:27 +0100

    Update GTM id

--- src/components/layout/Document/index.tsx
+++ src/components/layout/Document/index.tsx
@@ -27,14 +27,14 @@ export default function Document({ children, locale }: Props) {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5TPDQS6X');`}
+          })(window,document,'script','dataLayer','GTM-5QH4KDDB');`}
       </Script>
       <body
         className={`${fontStyles} bg-white antialiased container mx-auto px-4`}
       >
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5TPDQS6X"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5QH4KDDB"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
