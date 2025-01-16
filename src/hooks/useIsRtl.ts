c9e2c376c8955ee988847be5bb4b36885afb4848
Behnam Azimi <bhnmzm@gmail.com>
2025-01-16 22:52:48 +0100

    Refactor useRtl to make it SSR friendly

--- src/hooks/useIsRtl.ts
+++ src/hooks/useIsRtl.ts
@@ -1,5 +1,16 @@
 "use client";
 
-const useIsRtl = () => document.documentElement.dir === "rtl";
+import { LOCALE_COOKIE_NAME, rtlLocales } from "@/i18n/config";
+import { useCookies } from "react-cookie";
+
+const useIsRtl = () => {
+  const [{ [LOCALE_COOKIE_NAME]: locale }] = useCookies();
+
+  if (typeof window === "undefined") {
+    return rtlLocales.includes(locale);
+  }
+
+  return document.documentElement.dir === "rtl";
+};
 
 export default useIsRtl;
