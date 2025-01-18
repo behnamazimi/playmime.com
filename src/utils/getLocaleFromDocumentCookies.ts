dd8a752dfd50bfe010bf5d346fea66b09c706c8a
Behnam Azimi <bhnmzm@gmail.com>
2025-01-18 13:46:46 +0100

    Fix cookie initialization when document does not exist

--- src/utils/getLocaleFromDocumentCookies.ts
+++ src/utils/getLocaleFromDocumentCookies.ts
@@ -2,7 +2,7 @@ import { LOCALE_COOKIE_NAME } from "@/i18n/config";
 import { Cookies } from "react-cookie";
 
 const getLocaleFromDocumentCookies = () => {
-  const cookies = new Cookies(document.cookie);
+  const cookies = new Cookies(document ? document.cookie : null);
   return cookies.get(LOCALE_COOKIE_NAME);
 };
 
