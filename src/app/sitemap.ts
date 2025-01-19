e2c527664ae5ed115d8bd7689ef03ab7584fae37
Behnam Azimi <bhnmzm@gmail.com>
2025-01-19 10:14:09 +0100

    Add sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,58 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+  const lastModified = new Date();
+
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified,
+      changeFrequency: "monthly",
+      priority: 1,
+      alternates: {
+        languages: {
+          en: `${baseUrl}/en`,
+          fa: `${baseUrl}/fa`,
+        },
+      },
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified,
+      changeFrequency: "monthly",
+      priority: 0.8,
+      alternates: {
+        languages: {
+          en: `${baseUrl}/en/about`,
+          fa: `${baseUrl}/fa/about`,
+        },
+      },
+    },
+    {
+      url: `${baseUrl}/how-to-play`,
+      lastModified,
+      changeFrequency: "weekly",
+      priority: 0.8,
+      alternates: {
+        languages: {
+          en: `${baseUrl}/en/how-to-play`,
+          fa: `${baseUrl}/fa/how-to-play`,
+        },
+      },
+    },
+    {
+      url: `${baseUrl}/play`,
+      lastModified,
+      changeFrequency: "weekly",
+      priority: 0.7,
+      alternates: {
+        languages: {
+          en: `${baseUrl}/en/play`,
+          fa: `${baseUrl}/fa/play`,
+        },
+      },
+    },
+  ];
+}
