8d0ef57f3c52041b8fdd1c5a5c91c2b38c6008de
Behnam Azimi <bhnmzm@gmail.com>
2025-05-07 23:29:32 +0200

    Add new blog post

--- src/constants/blog.ts
+++ src/constants/blog.ts
@@ -5,6 +5,7 @@ export const BLOG_ITEMS: Record<Locale, string[]> = {
     "hard-charades-words-for-adults",
     "funny-charades-words-for-adults",
     "charades-rules-for-beginners",
+    "how-to-play-charades-with-kids-guide",
   ],
   fa: [],
 };
