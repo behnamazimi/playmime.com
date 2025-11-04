4ac227aa93f941155d5027f40684b790ea58749e
Behnam Azimi <bhnmzm@gmail.com>
2025-11-04 17:59:12 +0100

    refactor: remove commented code from Link component

--- src/i18n/routing/Link.tsx
+++ src/i18n/routing/Link.tsx
@@ -1,21 +1,4 @@
 "use client";
-// import NextLink from "next/link";
-// import { ComponentProps, FC } from "react";
-// import { defaultLocale, LOCALE_COOKIE_NAME } from "@/i18n/config";
-// import { useCookies } from "react-cookie";
-//
-// const Link: FC<ComponentProps<typeof NextLink>> = ({ href, ...props }) => {
-//   const [{ [LOCALE_COOKIE_NAME]: currentLocale }] = useCookies([
-//     LOCALE_COOKIE_NAME,
-//   ]);
-//
-//   const locale = currentLocale || defaultLocale;
-//   const hrefWithLocale = `/${locale}${href}`;
-//
-//   return <NextLink href={hrefWithLocale} {...props} />;
-// };
-//
-// export default Link;
 
 import { createNavigation } from "next-intl/navigation";
 import routing from "@/i18n/routing/index";
